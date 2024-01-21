4ca22cc83946f700377da0c03ad57f52c34d5816
Екатерина <150816212+Azazel1669@users.noreply.github.com>
2024-01-21 15:03:03 +0300

    Update Auarium.py

--- Auarium.py
+++ Auarium.py
@@ -10,7 +10,7 @@ from string import digits
 STATE, player = 'menu', ''
 try:
     def load_image(name, colorkey=None):
-        fullname = os.path.join(name)
+        fullname = os.path.join("data", name)
         if not os.path.isfile(fullname):
             print(f"Файл с изображением '{fullname}' не найден")
             sys.exit()
